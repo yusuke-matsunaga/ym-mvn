3a8c5f2dd007ed70047fe893f0bd34df4ba76937
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-10-08 15:09:39 +0900

    Doxygen 用にコメントを修正した．

--- include/ym/MvnBnConv.h
+++ include/ym/MvnBnConv.h
@@ -37,13 +37,13 @@ public:
   // メインの関数
   //////////////////////////////////////////////////////////////////////
 
-  /// @brief MvnMgr の内容を BdnMgr に変換する．
+  /// @brief MvnMgr の内容を BnNetwork に変換する．
   /// @param[in] mvmgr 対象の MvNetwork
-  /// @param[out] bdn 変換先の BdnMgr
+  /// @param[out] bnetwork 変換先の BnNetwork
   /// @param[out] mvnode_map 対応関係を格納するオブジェクト
   void
   operator()(const MvnMgr& mvmgr,
-	     BnBuilder& builder,
+	     BnNetwork& bnetwork,
 	     MvnBnMap& mvnode_map);
 
 
